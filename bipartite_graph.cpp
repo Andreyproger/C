b54965c0ca7982aec82e307de23f2525bb902eda
Andrey <crashc@yandex.ru>
2017-09-30 13:08:38 +0500

    Rewrite bipartite_graph.cpp
    1. Change function dfs:
        1) change algorithm in function;
        2) change the logic.
    2. Global variables moved to main.
    3. Organized right output.

--- bipartite_graph.cpp
+++ bipartite_graph.cpp
@@ -1,94 +1,108 @@
 #include <vector>
-#include <iostream>
+//#include <iostream>
 #include <fstream>
 #include <cmath>
 #include <algorithm>
+#include <iostream>
 using namespace std;
-//... Объявление переменных чтения, записи из файла (потоки чтения)
-ifstream input_file("in.txt");
-ofstream output_file("out.txt");
-vector < vector<int> > g;
-vector <int> color; // цвет вершины (0, 1, или 2)
-vector<int> time_in, time_out; // "времена" захода и выхода из вершины
- 
-int dfs_timer = 0; // "таймер" для определения времён
-int n, i, ch, vector_i, list_i;
- 
-bool dfs(int v)
-{
-    for (int i=1; i<g[v].size(); i++)
-    {
-        if (color[g[v][i]]==1 && color[v]==2 ||
-            color[g[v][i]]==2 && color[v]==1)
-            return false;
-        else if (color[v]==1) {
-            if (g[v][i] == 2)
-                continue;
-            color[g[v][i]] = 2;
-        }
-        else {
-            if (g[v][i] == 1)
-                continue;
-            color[g[v][i]] = 1;
-        }
-        return dfs(g[v][i]);
-    }
-    return true;
+
+int white = 0, red = 1, blue = 2;
+
+bool dfs(int v, vector <vector<int>> &g, vector<int> &color, int prevcolor){
+	if (color[v] != white){
+		return color[v] != prevcolor;
+	}
+	color[v] = prevcolor == red ? blue : red;
+	for (int i = 0; i < g[v].size(); i++){
+		int w = g[v][i];
+		//cout << w << endl;
+		if (!dfs(w, g, color, color[v])){
+			return false;
+		}
+	}
+	return true;
 }
- 
+
+//old version of dfs
+
+/*bool dfs(int v)
+{
+	
+	for (int i = 1; i<g[v].size(); ++i)
+	{
+		if (color[g[v][i]] == 1 && color[v] == 1 ||
+			color[g[v][i]] == 2 && color[v] == 2)
+			return false;
+		else if (color[v] == 1) {
+			if (g[v][i]!=0)
+				continue;
+			color[g[v][i]] = 2;
+			if (!dfs(g[v][i])) return false;
+		}
+		else {
+			if (g[v][i] != 0)
+				continue;
+			color[g[v][i]] = 1;
+			if (!dfs(g[v][i])) return false;
+		}
+		//return dfs(g[v][i]);
+	}
+	return true;
+}*/
+
+
+
 int main(){
+	ifstream input_file("in.txt");
+	ofstream output_file("out.txt");
+	int n;
 	input_file >> n;
-    for (i = 0; i <= n; i++){
-        color.push_back(0);
-    }
-    color[1]=1;
-    g.push_back(vector<int>());
-    for (int i = 1; i <= n; i++){
-        int t = -1;
-        g.push_back(vector<int>());
-        g[i].push_back(0);
-        while (true)
-        {
+	vector <int> color(n, white);
+	vector < vector<int> > g(n, vector<int>());
+
+	for (int i = 0; i < n; i++){
+		int t = -1;
+		while (true)
+		{
 			input_file >> t;
-            if (t == 0)
-                break;
-            g[i].push_back(t);
-        }
-    }
- 
-    bool ok = dfs(1);
- 
-    if (ok == true)
-    {
-		output_file<< "YES" << endl; //добавить вывод вершин входящих в доли графа
-        vector<int> white, black;
-        for (int i=1; i<=n; i++)
-            if (color[i]==1)
-                white.push_back(i);
-            else
-                black.push_back(i);
- 
-        sort(white.begin(), white.end());
-        sort(black.begin(), black.end());
- 
-        if (white[0]<black[0])
-        {
-            for (int i=0; i<white.size(); i++)
-				output_file << white[i] << " ";
-			output_file << endl << "0" << endl;
-            for (int i=0; i<black.size(); i++)
-				output_file << black[i] << " ";
-        }
-        else
-        {
-            for (int i=0; i<black.size(); i++)
-				output_file << black[i] << " ";
-			output_file << endl << "0" << endl;
-            for (int i=0; i<white.size(); i++)
-				output_file << white[i] << " ";
-        }
-    }
-    else
-		output_file  << "NO"; //добавить вывод вершин входящих в доли графа
-    return 0;
-    }
+			if (t == 0)
+				break;
+			g[i].push_back(t - 1);
+		}
+	}
+
+	bool ok = true;
+	for (int v = 0; v < n && ok; v++) {
+		ok = dfs(v, g, color, white);
+	}
+
+	cout << ok << endl;
+	if (ok)
+	{
+		output_file << "Y" << endl; //добавить вывод вершин входящих в доли графа
+		vector<int> reds, blues;
+		for (int i = 0; i < n; i++)
+			if (color[i] == red)
+				reds.push_back(i);
+			else
+				blues.push_back(i);
+
+		sort(reds.begin(), reds.end());
+		sort(blues.begin(), blues.end());
+
+		if ((reds.size() > 0 && blues.size() > 0 && blues[0] < reds[0]) || (blues.size() > 0 && reds.size() == 0)) {
+			reds.swap(blues);
+		}
+		
+		for (int i = 0; i<reds.size(); i++)
+			output_file << reds[i]+1 << " ";
+		output_file << "0" << endl;
+
+		for (int i = 0; i<blues.size(); i++)
+			output_file << blues[i]+1 << " ";
+		output_file << "0" << endl;
+	}
+	else
+		output_file << "N"; //добавить вывод вершин входящих в доли графа
+	return 0;
+}
